02a7db86256ce072c8fd8c67e0c33904d075d6d4
IsaacWane <627iw13@canonslade.tbft.uk>
2025-09-09 08:23:39 +0000

    new changes up to task 2d

--- Class Exercises/1 - Introduction to Javascript/Task 2a - Mark Percentage/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 2a - Mark Percentage/script.js
@@ -3,12 +3,14 @@ document.getElementById('calculateBtn').addEventListener('click', calculatePerce
 
 function calculatePercentage() {
     // TODO:
+   
     // 1. Get the maximum mark and student's mark from the input fields
     let maxMark = parseFloat(document.getElementById('maxMark').value);
     let studentMark = parseFloat(document.getElementById('studentMark').value);
-
+    
     // 2. Calculate the percentage using the formula: (student's mark ÷ maximum mark) × 100
     const percentage = (studentMark / maxMark) * 100 
+   
     // 3. Display the result in the percentage span element
     document.getElementById('percentage').textContent = `The percentage is: ${percentage }`;
 
